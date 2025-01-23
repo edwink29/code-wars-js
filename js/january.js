99c0b57847152f8f2ef04f4351dc730973c6cd27
Edwin Kurniawan <edwn.k29@gmail.com>
2025-01-23 22:40:48 +0700

    january code-wars

---
+++ js/january.js
@@ -0,0 +1,13 @@
+// // Januray, 16 2025.
+// function squareDigits(nums) {
+//   let digits = nums
+//     .toString()
+//     .split("")
+//     .map((n) => parseInt(n));
+//   let result = digits.map((n) => n * n);
+//   let hasil = parseInt(result.join(""));
+//   return hasil;
+// }
+
+// console.log(squareDigits(9119)); // Output: 5776
+// console.log(squareDigits(123));
